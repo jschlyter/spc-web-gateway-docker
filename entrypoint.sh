0631f24290d095d4f9b1123f75f87a7ccebac04d
Jakob Schlyter <jakob@kirei.se>
2018-06-10 19:33:07 +0200

    debug is optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-start-stop-daemon --start --chdir $SPC_GW_DIR -c $SPC_GW_USER --exec spc-web-gateway -- --debug
+start-stop-daemon --start --chdir $SPC_GW_DIR -c $SPC_GW_USER --exec spc-web-gateway -- $1
